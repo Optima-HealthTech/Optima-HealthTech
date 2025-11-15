f1f1b412e0394c51f299f94c8eb9fada550ac6ac
amine123-ux <mharo104@uottawa.ca>
2025-11-15 16:50:25 -0500

    teampage.js

--- webpage/teampage.js
+++ webpage/teampage.js
@@ -1,1 +1,13 @@
+const photos = document.querySelectorAll(".team-photo");
 
+photos.forEach(photo => {
+
+  photo.addEventListener("mouseenter", () => {
+    photo.classList.add("photo-hover");
+  });
+
+  photo.addEventListener("mouseleave", () => {
+    photo.classList.remove("photo-hover");
+  });
+
+});
